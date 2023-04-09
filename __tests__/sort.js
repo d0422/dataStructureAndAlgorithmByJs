84ba91e09c5482b39f35f9755487311200bc4604
d0422 <rlfehd2013@naver.com>
2023-04-10 00:29:09 +0900

    test: 테스트 데이터 추가

--- __tests__/sort.js
+++ __tests__/sort.js
@@ -10,6 +10,10 @@ const testData = [
     testInput: [5, 3, 1, 5, 3, 22],
     testAnswer: [1, 3, 3, 5, 5, 22],
   },
+  {
+    testInput: [5, 3, 100, 1, 5, 99, 3, 22],
+    testAnswer: [1, 3, 3, 5, 5, 22, 99, 100],
+  },
 ];
 
 test('선택정렬 테스트', () => {
@@ -19,18 +23,26 @@ test('선택정렬 테스트', () => {
 test('선택정렬 테스트2', () => {
   expect(selectionSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
 });
+test('선택정렬 테스트3', () => {
+  expect(selectionSort(testData[2].testInput)).toEqual(testData[2].testAnswer);
+});
 
 test('버블정렬 테스트', () => {
   expect(selectionSort(testData[0].testInput)).toEqual(testData[0].testAnswer);
 });
-test('버블정렬 테스트', () => {
+test('버블정렬 테스트2', () => {
   expect(bubbleSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
 });
-
+test('버블정렬 테스트3', () => {
+  expect(bubbleSort(testData[2].testInput)).toEqual(testData[2].testAnswer);
+});
 test('삽입정렬 테스트', () => {
   expect(insertSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
 });
 
 test('삽입정렬 테스트2', () => {
   expect(insertSort(testData[0].testInput)).toEqual(testData[0].testAnswer);
+});
+test('삽입정렬 테스트2', () => {
+  expect(insertSort(testData[2].testInput)).toEqual(testData[2].testAnswer);
 });
