eb742809a856d29f0592de1fdd487cf5cad996d9
d0422 <rlfehd2013@naver.com>
2023-04-10 00:25:04 +0900

    test: 삽입 정렬 테스트 코드 추가 및 코드 개선

--- __tests__/sort.js
+++ __tests__/sort.js
@@ -1,17 +1,36 @@
 const bubbleSort = require('../sort/bubbleSort');
+const insertSort = require('../sort/insertSort');
 const selectionSort = require('../sort/selectionSort');
+const testData = [
+  {
+    testInput: [5, 3, 1],
+    testAnswer: [1, 3, 5],
+  },
+  {
+    testInput: [5, 3, 1, 5, 3, 22],
+    testAnswer: [1, 3, 3, 5, 5, 22],
+  },
+];
 
 test('선택정렬 테스트', () => {
-  expect(selectionSort([5, 3, 1])).toEqual([1, 3, 5]);
+  expect(selectionSort(testData[0].testInput)).toEqual(testData[0].testAnswer);
 });
 
 test('선택정렬 테스트2', () => {
-  expect(selectionSort([5, 3, 1, 5, 3, 22])).toEqual([1, 3, 3, 5, 5, 22]);
+  expect(selectionSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
 });
 
 test('버블정렬 테스트', () => {
-  expect(selectionSort([5, 3, 1])).toEqual([1, 3, 5]);
+  expect(selectionSort(testData[0].testInput)).toEqual(testData[0].testAnswer);
 });
 test('버블정렬 테스트', () => {
-  expect(bubbleSort([5, 3, 1, 5, 3, 22])).toEqual([1, 3, 3, 5, 5, 22]);
+  expect(bubbleSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
+});
+
+test('삽입정렬 테스트', () => {
+  expect(insertSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
+});
+
+test('삽입정렬 테스트2', () => {
+  expect(insertSort(testData[0].testInput)).toEqual(testData[0].testAnswer);
 });
