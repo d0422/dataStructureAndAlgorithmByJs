84155f5484e5f104942c4de3b785e20d320714fb
d0422 <rlfehd2013@naver.com>
2023-04-09 17:05:30 +0900

    test: 선택정렬 테스트 코드 추가

--- __tests__/sort.js
+++ __tests__/sort.js
@@ -3,3 +3,7 @@ const selectionSort = require('../sort/selectionSort');
 test('선택정렬 테스트', () => {
   expect(selectionSort([5, 3, 1])).toEqual([1, 3, 5]);
 });
+
+test('선택정렬 테스트2', () => {
+  expect(selectionSort([5, 3, 1, 5, 3, 22])).toEqual([1, 3, 3, 5, 5, 22]);
+});
