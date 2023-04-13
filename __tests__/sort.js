e9e2633362ba55c9de246a497567f94c30830f41
d0422 <rlfehd2013@naver.com>
2023-04-13 15:22:24 +0900

    test: heapSort 테스트 코드 작성

--- __tests__/sort.js
+++ __tests__/sort.js
@@ -1,4 +1,5 @@
 const bubbleSort = require('../sort/bubbleSort');
+const heapSort = require('../sort/heapSort');
 const insertSort = require('../sort/insertSort');
 const mergeExecute = require('../sort/mergeSort');
 const QuickSortExecute = require('../sort/quickSort');
@@ -76,4 +77,14 @@ test('퀵정렬 테스트', () => {
   expect(QuickSortExecute([5, 3, 100, 1, 5, 99, 3, 22])).toEqual([
     1, 3, 3, 5, 5, 22, 99, 100,
   ]);
+});
+
+test('힙소트 테스트', () => {
+  expect(heapSort(testData[0].testInput)).toEqual(testData[0].testAnswer);
+});
+test('힙소트 테스트', () => {
+  expect(heapSort(testData[1].testInput)).toEqual(testData[1].testAnswer);
+});
+test('힙소트 테스트', () => {
+  expect(heapSort(testData[2].testInput)).toEqual(testData[2].testAnswer);
 });
