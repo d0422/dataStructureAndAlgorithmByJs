2958d869c175df9f4e21f0fd31f49c57a61b6996
d0422 <rlfehd2013@naver.com>
2023-04-13 19:07:42 +0900

    test: 후위순회 테스트 코드 작성

--- __tests__/tree.js
+++ __tests__/tree.js
@@ -1,4 +1,5 @@
 const inOrder = require('../dataStructure/tree/inOrder');
+const postOrder = require('../dataStructure/tree/postOrder');
 const preOrder = require('../dataStructure/tree/preOrder');
 
 test('전위순회 테스트', () => {
@@ -15,6 +16,15 @@ test('중위순회 테스트', () => {
   const output = [[1], [2], [3], [5], [7], [10]];
   const consoleSpy = jest.spyOn(console, 'log');
   inOrder(inputArray);
+  expect(consoleSpy.mock.calls).toEqual(output);
+  consoleSpy.mockClear();
+});
+
+test('후위순회 테스트', () => {
+  const inputArray = [7, 5, 10, 2, 1, 3];
+  const output = [[10], [3], [1], [2], [5], [7]];
+  const consoleSpy = jest.spyOn(console, 'log');
+  postOrder(inputArray);
   expect(consoleSpy.mock.calls).toEqual(output);
   consoleSpy.mockClear();
 });
