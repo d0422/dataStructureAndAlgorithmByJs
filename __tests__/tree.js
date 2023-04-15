12097f27afea2ee9f87c1eb4a04143f1f5ea49ba
d0422 <rlfehd2013@naver.com>
2023-04-15 10:23:18 +0900

    test: 트리 삭제 테스트케이스 추가

--- __tests__/tree.js
+++ __tests__/tree.js
@@ -59,14 +59,55 @@ test('트리 삭제 테스트, 자식노드가 둘인 경우', () => {
 });
 
 test('트리 삭제 테스트, 자식노드가 왼쪽 하나인 경우', () => {
-  const inputArray = [7, 5, 10, 2, 1, 3];
-  const output = [[1], [3], [5], [7], [10]];
+  const inputArray = [12, 8, 7, 1, 10, 9, 11, 23, 14, 13, 15, 25];
+  const output = [
+    [1],
+    [8],
+    [9],
+    [10],
+    [11],
+    [12],
+    [13],
+    [14],
+    [15],
+    [23],
+    [25],
+  ];
+
   const consoleSpy = jest.spyOn(console, 'log');
   const tree = new BinaryTree();
   inputArray.forEach((number) => {
     tree.add(number);
   });
-  tree.delete(2);
+  tree.delete(7);
+  tree.inOrder(tree.root);
+  expect(consoleSpy.mock.calls).toEqual(output);
+  consoleSpy.mockClear();
+});
+
+test('트리 삭제 테스트, 자식노드가 오른쪽 하나인 경우', () => {
+  const inputArray = [12, 8, 7, 1, 10, 9, 11, 23, 14, 13, 15, 25];
+  const output = [[1], [7], [8], [9], [10], [11], [12], [13], [14], [15], [25]];
+  const consoleSpy = jest.spyOn(console, 'log');
+  const tree = new BinaryTree();
+  inputArray.forEach((number) => {
+    tree.add(number);
+  });
+  tree.delete(23);
+  tree.inOrder(tree.root);
+  expect(consoleSpy.mock.calls).toEqual(output);
+  consoleSpy.mockClear();
+});
+
+test('트리 삭제 테스트, 자식노드가 없는 경우', () => {
+  const inputArray = [7, 5, 10, 2, 1, 3];
+  const output = [[1], [2], [3], [5], [7]];
+  const consoleSpy = jest.spyOn(console, 'log');
+  const tree = new BinaryTree();
+  inputArray.forEach((number) => {
+    tree.add(number);
+  });
+  tree.delete(10);
   tree.inOrder(tree.root);
   expect(consoleSpy.mock.calls).toEqual(output);
   consoleSpy.mockClear();
