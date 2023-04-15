74b794b243e4a9ca7b087f48cc816abae833dc6c
d0422 <rlfehd2013@naver.com>
2023-04-15 10:00:55 +0900

    fix: add 제대로 되지않던 문제 해결

--- dataStructure/tree/BinaryTree.js
+++ dataStructure/tree/BinaryTree.js
@@ -9,11 +9,14 @@ class BinaryTree {
     if (!this.root) this.root = new BinaryNode(value);
     else {
       let currentNode = this.root;
-      while (currentNode.left != null && currentNode.right != null) {
+      while (
+        (currentNode.left != null && currentNode.value > value) ||
+        (currentNode.right != null && currentNode.value < value)
+      ) {
         while (currentNode.value > value && currentNode.left != null) {
           currentNode = currentNode.left;
         }
-        while (currentNode.value <= value && currentNode.right != null) {
+        while (currentNode.value < value && currentNode.right != null) {
           currentNode = currentNode.right;
         }
       }
@@ -26,20 +29,39 @@ class BinaryTree {
   }
 
   delete(value) {
-    let currentNode = this.root;
-    while (currentNode.value != value) {
-      if (currentNode.left == null && currentNode.right == null) {
-        return null;
-      }
-      if (currentNode.value > value && currentNode.left != null) {
-        currentNode = currentNode.left;
-      }
-      if (currentNode.value < value && currentNode.right != null) {
-        currentNode = currentNode.right;
-      }
-      if (currentNode.value === value) {
-      }
+    if (this.root.value === value && !this.root.left && !this.root.right) {
+      this.root = null;
+      return;
+    } else {
+      this.deleteNode(this.root, value);
     }
+  }
+
+  getMinNode(node) {
+    let current = node;
+    while (current.left != null) {
+      current = current.left;
+    }
+    return current;
+  }
+
+  deleteNode(currentNode, value) {
+    if (currentNode.value < value) {
+      currentNode.right = this.deleteNode(currentNode.right, value);
+    } else if (currentNode.value > value) {
+      currentNode.left = this.deleteNode(currentNode.left, value);
+    } else {
+      if (!currentNode.left) {
+        return currentNode.right;
+      }
+      if (!currentNode.right) {
+        return currentNode.left;
+      }
+      const minNode = this.getMinNode(currentNode.right);
+      currentNode.value = minNode.value;
+      currentNode.right = this.deleteNode(currentNode.right, minNode.value);
+    }
+    return currentNode;
   }
 
   preOrder(currentNode) {
