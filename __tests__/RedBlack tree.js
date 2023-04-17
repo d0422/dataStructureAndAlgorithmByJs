1799814b9e844905235bcdde81766e6c118377cc
d0422 <rlfehd2013@naver.com>
2023-04-17 16:30:27 +0900

    test: 추가 테스트 구성

--- __tests__/RedBlack tree.js
+++ __tests__/RedBlack tree.js
@@ -32,3 +32,49 @@ test('레드블랙트리 recoloring 테스트', () => {
     tree.root.left.right.value,
   ]).toEqual(['black', 'black', 'black', 'red', 5, 2, 7, 3]);
 });
+
+test('레드블랙 트리 테스트', () => {
+  const tree = new RedBlackTree();
+  [10, 5, 20, 3, 7, 15, 25, 1, 4].forEach((num) => {
+    tree.add(num);
+  });
+  expect([
+    tree.root.color,
+    tree.root.left.color,
+    tree.root.right.color,
+    tree.root.left.left.color,
+    tree.root.left.right.color,
+    tree.root.left.left.left.color,
+    tree.root.left.left.right.color,
+    tree.root.right.left.color,
+    tree.root.right.right.color,
+    tree.root.value,
+    tree.root.left.value,
+    tree.root.right.value,
+    tree.root.left.left.value,
+    tree.root.left.right.value,
+    tree.root.left.left.left.value,
+    tree.root.left.left.right.value,
+    tree.root.right.left.value,
+    tree.root.right.right.value,
+  ]).toEqual([
+    'black',
+    'red',
+    'black',
+    'black',
+    'black',
+    'red',
+    'red',
+    'red',
+    'red',
+    10,
+    5,
+    20,
+    3,
+    7,
+    1,
+    4,
+    15,
+    25,
+  ]);
+});
